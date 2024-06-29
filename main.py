3dd12e4492a67b23c52b19bd0c0aeb8edf17e284
Anderson Ortiz <ortizone620@gmail.com>
2024-06-29 05:08:10 -0600

    user schema

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+#https://fastapi.tiangolo.com/
+
+#https://pymongo.readthedocs.io/en/stable/examples/index.html
+
+#https://github.com/AnderOrtiz/e-comerse
